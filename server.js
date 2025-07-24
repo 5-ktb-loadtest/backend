146278a1456626826132f2ae5165349ec51da326
YuSung011017 <salamander011017@gmail.com>
2025-07-24 01:09:49 +0000

    feat : Dockerfile 및 cors 수정

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const corsOptions = {
     'https://localhost:3001',
     'https://localhost:3002',
     'http://0.0.0.0:3000',
-    'https://0.0.0.0:3000'
+    'https://0.0.0.0:3000',
+    'http://34.64.60.60:3000'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
